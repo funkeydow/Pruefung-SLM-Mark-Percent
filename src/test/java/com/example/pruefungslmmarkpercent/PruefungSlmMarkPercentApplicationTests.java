6532762bd0214f1c0de02cbaed63a3822131e5dd
funkeydow <funkeydow@pm.me>
2022-12-21 18:12:51 +0100

    Initial Class Creation

--- src/test/java/com/example/pruefungslmmarkpercent/PruefungSlmMarkPercentApplicationTests.java
+++ src/test/java/com/example/pruefungslmmarkpercent/PruefungSlmMarkPercentApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PruefungSlmMarkPercentApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
