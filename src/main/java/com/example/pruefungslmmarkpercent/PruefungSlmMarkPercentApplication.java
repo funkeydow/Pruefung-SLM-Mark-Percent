be43992cf895148bd6b428e2ffb70b136c58c522
funkeydow <funkeydow@pm.me>
2022-12-21 17:59:35 +0100

    Initial commit

---
+++ src/main/java/com/example/pruefungslmmarkpercent/PruefungSlmMarkPercentApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruefungslmmarkpercent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruefungSlmMarkPercentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruefungSlmMarkPercentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pruefungslmmarkpercent/PruefungSlmMarkPercentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruefungslmmarkpercent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruefungSlmMarkPercentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
