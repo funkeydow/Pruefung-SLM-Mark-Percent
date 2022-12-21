3e19ecd0c14f8a878f2ed40fd8a05dc11e8c6f38
funkeydow <funkeydow@pm.me>
2022-12-21 18:26:37 +0100

    Added feature for api/mark for returning the mark to a certain percentage value

--- src/main/java/com/example/pruefungslmmarkpercent/PruefungSlmMarkPercentApplication.java
+++ src/main/java/com/example/pruefungslmmarkpercent/PruefungSlmMarkPercentApplication.java
@@ -2,7 +2,11 @@ package com.example.pruefungslmmarkpercent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class PruefungSlmMarkPercentApplication {
 
@@ -10,4 +14,26 @@ public class PruefungSlmMarkPercentApplication {
         SpringApplication.run(PruefungSlmMarkPercentApplication.class, args);
     }
 
+    @GetMapping("api/mark")
+    public String SetMessage(@RequestParam(name="percentage") String msg){
+        int percentage = Integer.valueOf(msg);
+        if(percentage >= 88){
+            return "1";
+        }
+        else if (percentage >= 75 && percentage < 88){
+            return "2";
+        }
+        else if (percentage >= 63 && percentage < 75){
+            return "3";
+        }
+        else if (percentage >= 50 && percentage < 63){
+            return "4";
+        }
+        else if (percentage < 50){
+            return "5";
+        }
+        else{
+            return "Your value wasn´t in the allowed range";
+        }
+    }
 }
